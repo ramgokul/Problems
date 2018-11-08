b06e381caeec4fd72590012e5a063b5c36c08b4f
RamnathGokul <ramnath1109@gmail.com>
2018-11-08 17:11:13 +0530

    commit

--- Practice/src/problems/StringProblems/batch1/CountSubStrWithSameFirstNLastChars.java
+++ Practice/src/problems/StringProblems/batch1/CountSubStrWithSameFirstNLastChars.java
@@ -3,6 +3,26 @@ package problems.StringProblems.batch1;
 import java.util.Arrays;
 
 public class CountSubStrWithSameFirstNLastChars {
+    /*
+        We are given a string S, we need to find count of all contiguous substrings starting and ending with same character.
+
+        Examples :
+
+        Input  : S = "abcab"
+        Output : 7
+        There are 15 substrings of "abcab"
+        a, ab, abc, abca, abcab, b, bc, bca
+        bcab, c, ca, cab, a, ab, b
+        Out of the above substrings, there
+        are 7 substrings : a, abca, b, bcab,
+        c, a and b.
+
+        Input  : S = "aba"
+        Output : 4
+        The substrings are a, b, a and aba
+
+        Method 3 (Best approach) : Now if we carefully observe then we can realize that the answer just depends on frequencies of characters in the original string. For example in string abcab, frequency of ‘a’ is 2 and substrings contributing to answer are a, abca and a respectively, a total of 3, which is calculated by (frequency of ‘a’+1)C2.
+     */
     static final int MAX_CHAR = 26;
 
 
--- Practice/src/problems/StringProblems/batch4/PrintAllSubstringOfAGivenString.java
+++ Practice/src/problems/StringProblems/batch4/PrintAllSubstringOfAGivenString.java
@@ -21,7 +21,7 @@ public class PrintAllSubstringOfAGivenString {
      */
 
     public static void main(String[] args) {
-        String s = "abcbabcb";
+        String s = "this is a test string";
 
         for (int i=0; i<s.length(); i++){
             for (int j=i+1; j<=s.length(); j++){
